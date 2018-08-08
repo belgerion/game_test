796313619b92feb6511042bcebda5aa5ccac5980
Jeffrey Barnes <belgerion@gmail.com>
2018-08-07 22:40:01 -0600

    bug fix

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
         for (var i = 1; i <= (userX * userY); i++){
             let random = Math.round(((Math.random() * 10) * 3) / 10);
             $('#gameSpace').append('<div id="hex' + i + '" class="hex col-lg-1">' + i + '<br>Height:' + random + '</div>');
-            document.getElementById('hex'+i).style.backgroundImage = "url('./board_tiles/biggrass" + random + ".jpg')";
+            document.getElementById('hex'+i).style.backgroundImage = "url('../board_tiles/biggrass" + random + ".jpg')";
             console.log(random);
         };
     })
-});
+});https://github.com/belgerion/game_test/blob/master/board_tiles/BigGrass1.jpg?raw=true
